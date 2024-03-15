7f6af78910760be7cd37ab598972818b728b2fef
AcrylicShrimp <led789zxpp@naver.com>
2024-03-15 21:35:37 +0900

    test: add staging file tests

--- src/routes/staging_file/tests.rs
+++ src/routes/staging_file/tests.rs
@@ -75,3 +75,44 @@ async fn test_create_staging_file() {
 
     assert_eq!(raw_staging_file, created_staging_file);
 }
+
+#[rocket::async_test]
+async fn test_remove_staging_file() {
+    let (rocket, _database_dropper) = create_test_rocket_instance().await;
+    let client = Client::tracked(rocket).await.unwrap();
+    let auth_service = client.rocket().state::<Arc<AuthService>>().unwrap();
+    let staging_file_service = client.rocket().state::<Arc<StagingFileService>>().unwrap();
+    let user_service = client.rocket().state::<Arc<UserService>>().unwrap();
+
+    let (_initial_user, initial_user_session) =
+        create_initial_user(auth_service, user_service).await;
+
+    let staging_file = staging_file_service
+        .create_staging_file("staging_file", Some("video/mp4"))
+        .await
+        .unwrap();
+
+    let response = client
+        .delete(format!("/staging-files/{}", staging_file.id))
+        .header(Accept::JSON)
+        .header(ContentType::JSON)
+        .header(Header::new(
+            "Authorization",
+            format!("Bearer {}", initial_user_session.token),
+        ))
+        .dispatch()
+        .await;
+
+    let status = response.status();
+    let removed_staging_file = response.into_json::<StagingFile>().await.unwrap();
+
+    assert_eq!(status, Status::Ok);
+    assert_eq!(removed_staging_file, staging_file);
+
+    let raw_removed_staging_file = staging_file_service
+        .get_staging_file_by_id(removed_staging_file.id)
+        .await
+        .unwrap();
+
+    assert_eq!(raw_removed_staging_file, None);
+}
